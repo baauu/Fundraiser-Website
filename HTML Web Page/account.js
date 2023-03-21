16f7c2f7f86dffa5fd327169c66a4531350a5c39
baauu <jbr75@umsystem.edu>
2023-03-21 16:38:30 -0500

    updates

--- HTML Web Page/account.js
+++ HTML Web Page/account.js
@@ -1,59 +1,59 @@
 
-import firebase from "firebase/app";
+// import firebase from "firebase/app";
 
-const firebaseConfig = {
-    //   copy your firebase config informations
-    apiKey: "AIzaSyC83qnIugk6Gtne0-NvHJZuJo9v25c29hw",
-    authDomain: "capstone-fund-website.firebaseapp.com",
-    databaseURL: "https://capstone-fund-website-default-rtdb.firebaseio.com",
-    projectId: "capstone-fund-website",
-    storageBucket: "capstone-fund-website.appspot.com",
-    messagingSenderId: "261919550921",
-    appId: "1:261919550921:web:686845ffc25aaf78074c18",
-    measurementId: "G-1J8HQKC1YN"
-  };
+// const firebaseConfig = {
+//     //   copy your firebase config informations
+//     apiKey: "AIzaSyC83qnIugk6Gtne0-NvHJZuJo9v25c29hw",
+//     authDomain: "capstone-fund-website.firebaseapp.com",
+//     databaseURL: "https://capstone-fund-website-default-rtdb.firebaseio.com",
+//     projectId: "capstone-fund-website",
+//     storageBucket: "capstone-fund-website.appspot.com",
+//     messagingSenderId: "261919550921",
+//     appId: "1:261919550921:web:686845ffc25aaf78074c18",
+//     measurementId: "G-1J8HQKC1YN"
+//   };
   
-  // initialize firebase
+//   // initialize firebase
   
-  firebase.initializeApp(firebaseConfig);
-  //const app = initializeApp(firebaseConfig);
+//   firebase.initializeApp(firebaseConfig);
+//   //const app = initializeApp(firebaseConfig);
   
-  // reference your database
-  var acctDB = firebase.database().ref("signup-form");
+//   // reference your database
+//   var acctDB = firebase.database().ref("signup-form");
   
-  document.getElementById("signup-form").addEventListener('submit', submitForm);
+//   document.getElementById("signup-form").addEventListener('submit', submitForm);
   
-  function submitForm(e) {
-    e.preventDefault();
+//   function submitForm(e) {
+//     //e.preventDefault();
     
-    var emailid = getElementVal("inputEmail");
-    var PWD = getElementVal("inputPassword");
+//     var emailid = getElementVal("inputEmail");
+//     var PWD = getElementVal("inputPassword");
   
-    //saveMessages(emailid, PWD); // have to unlock later
+//     //saveMessages(emailid, PWD); // have to unlock later
 
-    console.log(emailid, PWD);
+//     console.log(emailid, PWD);
   
-    //   enable alert
-    //document.querySelector(".alert").style.display = "block";
+//     //   enable alert
+//     //document.querySelector(".alert").style.display = "block";
   
-    //   remove the alert
-    // setTimeout(() => {
-    //   document.querySelector(".alert").style.display = "none";
-    // }, 3000);
+//     //   remove the alert
+//     // setTimeout(() => {
+//     //   document.querySelector(".alert").style.display = "none";
+//     // }, 3000);
   
-    //   reset the form
-//     document.getElementById("contactForm").reset();
-  };
+//     //   reset the form
+// //     document.getElementById("contactForm").reset();
+//   };
   
-  const saveMessages = (emailid, PWD) => {
-    var newContactForm = acctDB.push();
+//   // const saveMessages = (emailid, PWD) => {
+//   //   var newContactForm = acctDB.push();
   
-    newContactForm.set({
-      email: emailid,
-      password: PWD,
-    });
-  };
+//   //   newContactForm.set({
+//   //     email: emailid,
+//   //     password: PWD,
+//   //   });
+//   // };
   
-  const getElementVal = (id) => {
-    return document.getElementById(id).value;
-  };
+//   const getElementVal = (id) => {
+//     return document.getElementById(id).value;
+//   };
