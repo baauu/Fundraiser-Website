9978e9d4f77421348423301d57b48b0fedaf517e
baauu <jbr75@umsystem.edu>
2023-04-14 10:20:51 -0500

    update

---
+++ HTML Web Page V2/signup.js
@@ -0,0 +1,15 @@
+// import { app } from "./firebaseConfig";
+
+
+// const email = document.getElementById("email")
+// const password = document.getElementById("password")
+
+// const submitBtn = document.getElementById("submitBtn")
+
+// submitBtn.addEventListener("click", function() {
+//     let emailValue = email.value
+//     let passwordValue = password.value
+
+//     console.log(emailValue),
+//     console.log(passwordValue)
+// })
