2173fdccd281557d987cbf373b90df11f6993274
baauu <jbr75@umsystem.edu>
2023-04-23 16:17:46 -0500

    Update user.js

--- HTML Web Page V2/user.js
+++ HTML Web Page V2/user.js
@@ -1,6 +1,5 @@
-
     import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
-    import { getDatabase, ref, get, set, child, update, remove } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js";
+    // import { getDatabase, ref, get, set, child, update, remove } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js";
     import { getAuth, createUserWithEmailAndPassword, 
         signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
 
@@ -17,33 +16,50 @@
 
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
-    //const db = getDatabase();
 
     var email = document.querySelector("#email");
     var password = document.querySelector("#password");
 
-    var submitBtn = document.querySelector("#submitBtn");
-    //var signoutBtn = document.getElementById("signOut").style.visibility = "visible";
-    // document.querySelector("#signOut");
+    var signInBtn = document.querySelector("#signInBtn");
+    var signoutBtn = document.querySelector("#signout-nav");
+
+    var currentUser = "";
+
+
+
+    onAuthStateChanged(auth, user => {
+        if(user) /*if user exists*/{
+            //add the sign out button to account html bar
+            //make account tab visible
+            console.log('logged in', user);
+            const userID = auth.currentUser;
+            const uid = userID.uid;
+            currentUser = uid;
+            signInNav();
+            
+        }
+        else {
+            console.log('logged OUT', user);
+            signOutNav();
+        }
+    })
 
 
     const userSignIn = async() => {
         const emailVal = email.value;
         const passVal = password.value;
+
         signInWithEmailAndPassword(auth, emailVal, passVal)
-        .then((userCredential) => {
-            nav();
-            const user = userCredential.user;
+        .then(() => {
+            signInNav();
+            // checkAuthState();
+            // const user = userCredential.user;
             const userID = auth.currentUser;
             const uid = userID.uid;
+            currentUser = uid;
 
             // submitBtn.href = "account.html?" + uid;
             sessionStorage.setItem("userID", uid);
-
-
-            console.info(uid);
-            //addURL(uid);
-
             alert("You have signed in successfully!");
 
         })
@@ -55,17 +71,25 @@
         })
     }
 
-    function nav (){
+    function signInNav (){
         document.getElementById("signout-nav").style.visibility = "visible";
         document.getElementById("myaccount").style.visibility = "visible";
-
+        document.getElementById("login-nav").style.visibility = "hidden";
+    }
+    function signOutNav () {
+        document.getElementById("signout-nav").style.visibility = "hidden";
+        document.getElementById("myaccount").style.visibility = "hidden";
+        document.getElementById("login-nav").style.visibility = "visible";
     }
 
 
     const userSignOut = async() => {
-        signOut(auth).then(() => {
+        signOut(auth)
+            .then(() => {
             // Sign-out successful.
             alert("signed out")
+            signOutNav();
+
           }).catch((error) => {
             // An error happened.
             const errorCode = error.code;
@@ -74,29 +98,30 @@
           });
     }
 
-    //function that helped me trouble shoot
-    function display() {
-        let emailVal = email.value
-        
-        console.log(emailVal)
+
+    // const checkAuthState = async() => {
+    //     onAuthStateChanged(auth, user => {
+    //         if(user) /*if user exists*/{
+    //             //add the sign out button to account html bar
+    //             //make account tab visible
+    //             signInNav();
+                
+    //         }
+    //         else {
+                
+    //         }
+    //     })
+    // }
+
+    if(signoutBtn) {
+        signoutBtn.addEventListener('click', userSignOut);
     }
 
-    const checkAuthState = async() => {
-        onAuthStateChanged(auth, user => {
-            if(user) /*if user exists*/{
-                //add the sign out button to account html bar
-                //make account tab visible
-                
-                console.log("user has logged in")
-            }
-            else {
-                
-            }
-        })
+    if(signInBtn) {
+        signInBtn.addEventListener('click', userSignIn);
     }
-
-
-    submitBtn.addEventListener('click', userSignIn);
-    //signoutBtn.addEventListener('click', userSignOut);
+    
+    
+    
     
     
